1e6af74712fc0747c45ed85ac705a618fcfb9126
Vladimir Polyakov <vlpolyakov@mai.ru>
2021-05-10 03:00:00 +0300

    Проектное задание неделя 2 доработка 1

--- carts/views.py
+++ carts/views.py
@@ -1,29 +1,34 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from carts.models import Cart, CartItem
 from carts.paginators import CartItemLimitOffsetPagination
-from carts.serializers import CartSerializer, CartItemSerializer, CartItemUpdateSerializer
+from carts.serializers import CartSerializer, CartItemSerializer, CartItemCreateEditSerializer
 
 
-class CartListView(ListAPIView):
+class CartView(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = CartSerializer
 
-    def get_queryset(self):
-        return Cart.objects.filter(user=self.request.user)
+    def get_object(self):
+        cart = self.request.user.cart
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, cart)
+
+        return cart
 
 
 class CartItemViewSet(ModelViewSet):
     permission_classes = (IsAuthenticated,)
     pagination_class = CartItemLimitOffsetPagination
+    serializer_class = CartItemSerializer
 
     def get_queryset(self):
-        return CartItem.objects.filter(cart=self.request.user.cart())
+        return self.request.user.cart.cart_items.all()
 
     def get_serializer_class(self):
-        if self.request._request.method in ['PUT', 'PATCH']:
-            return CartItemUpdateSerializer
+        if self.request._request.method in ['POST', 'PUT', 'PATCH']:
+            return CartItemCreateEditSerializer
         else:
             return CartItemSerializer
